dcecf191c69fcbc02866d64a0e62b8b2c1eef0bf
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-29 18:10:41 +0100

    Question 2 terminer:Créer un ensemble contenant 0 à 10 en utilisant une boucle

--- Exo1.js
+++ Exo1.js
@@ -1,3 +1,12 @@
 // Reponse numero 1
 const ensemble = new Set()
 console.log(ensemble);
+
+
+// Reponse numero 2
+const number = [0,1,2,3,4,5,6,7,8,9,10]
+ setOfNumber = new Set()
+ for (const numero of number) {
+    setOfNumber.add(numero)
+ }
+console.log(setOfNumber);
