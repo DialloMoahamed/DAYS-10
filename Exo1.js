ee2cb08c7a0219de783aa99e38b532ce49d856ce
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-29 19:18:08 +0100

    Question 5 terminer:Créer un ensemble de 5 éléments de chaîne à partir d'un tableau

--- Exo1.js
+++ Exo1.js
@@ -19,3 +19,15 @@ console.log(setOfNumber.delete(0));
 // Reponse numero 4
 setOfNumber.clear();
 console.log(setOfNumber);
+
+
+// Reponse numero 5
+const element = new Map();
+element.set('Niger', 'Niamey')
+element.set('Togo', 'Lomé')
+element.set('Nigeria', 'Abuja')
+element.set('Bénin', 'Porto-Novo')
+element.set('Burkina fasso', 'Ouagadougou')
+for (const [country, city] of element) {
+    console.log(country, city);
+}
